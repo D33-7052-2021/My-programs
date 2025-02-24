6269bec8f479307939a219fc544f5f2d13a1815e
D33-7052-2021 <anthonymutuku81@gmail.com>
2025-02-24 14:49:02 -0800

    Add files via upload

---
+++ antony area.c
@@ -0,0 +1,20 @@
+//program to find the area of rectangle.
+// the user is required to input the length and width of the rectangle.
+/*
+Author:Anthony Mutuku
+Registration no:D33-7052-2021
+Date:23/02/2025
+*/
+#include<stdio.h>
+int main() {
+  int length=0;
+  int width=0;
+  printf("length:\n");
+  scanf("%d",&length);//input length
+  printf("width:\n");
+  scanf("%d",&width);//input width
+  int area= length *width;
+  printf("area:%d\n",area);// output the area based on the formula A=L*W 
+  return 0;
+}
+//the user can add different values to get different values of A.
